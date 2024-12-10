7425e0592a18182a61e3240b59b30f626e93808f
DaiDiT <darryl.zaidra@gmail.com>
2024-12-11 00:44:48 +0700

    lil update

--- routes/index.route.js
+++ routes/index.route.js
@@ -5,6 +5,13 @@ const fotoKosanRoute = require("./foto-kosan.route.js")
 
 const router = express.Router()
 
+router.get("/status", (req, res) => {
+    res.status(200).json({
+        statusCode: 200,
+        message: "Server is running"
+    });
+});
+
 router.use("/admin", adminRoute)
 router.use("/kosan", kosanRoute)
 router.use("/foto", fotoKosanRoute)
